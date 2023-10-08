68cb99cedb282de8476cca497247bf82efef356d
Willian <brcraftgames@gmail.com>
2023-10-08 01:10:20 -0300

    carrinho se atualiza

--- public/script/script.js
+++ public/script/script.js
@@ -1,4 +1,19 @@
-// Função para criar produtos e as divs das classes se necessário
+let item;
+
+sleep(300).then(() => {
+    refreshCar()
+});
+
+function getItem(){
+    if(localStorage.getItem("item") != null){
+        item = localStorage.getItem("item");
+        item = JSON.parse(item);
+    }else{
+        item = [];
+    }
+    return item;
+}
+
 function createProduct(Name, Price, Img, Class, Id, isOnly) {
     // criar div por classe
     if(document.getElementById(Class) == null){
@@ -21,6 +36,7 @@ function createProduct(Name, Price, Img, Class, Id, isOnly) {
     //  isOnly ? addProductOnly(Name, Price, Img, Class, Id) : addProduct(Name, Price, Img, Class, Id);
     addProduct(Name, Price, Img, Class, Id, isOnly ? true : false);
 }
+
 function addProduct(Name, Price, Img, Class, Id, isOnly){
     $(`#${Class}`).children(".scroll-effect").children(`.product-cards-cotainer${isOnly ? "-only": ""}`)
     .append(`
@@ -36,9 +52,28 @@ function addProduct(Name, Price, Img, Class, Id, isOnly){
             </a>
             `)
 }
+
+function sleep(ms) {
+    return new Promise(resolve => setTimeout(resolve, ms));
+}
+
+function refreshCar(){
+    let qtdP = 0;
+    let item = getItem();
+
+    item.forEach(i => {
+        qtdP += parseInt(i.qtd);
+    });
+
+    document.getElementById("littlecar").innerHTML = qtdP + "<br>produto(s)";
+    console.log(qtdP);
+}
+
 function addLittleCar(){
     let isInside = false;
     let index = 0;
+    let item = getItem();
+
     item.findIndex(function(x, i){
         if(x.id == window.location.href.split("/")[4]){
             isInside = true;
@@ -59,7 +94,9 @@ function addLittleCar(){
         item[index].qtd += 1
     }
     localStorage.setItem("item", JSON.stringify(item));
+    refreshCar();
 }
+
 function scrollL(father){
     $(`#${father}`)
     .children(".scroll-effect")
@@ -67,6 +104,7 @@ function scrollL(father){
     .children(".scroll-effect")
     .children(".product-cards-cotainer").scrollLeft() + 150);
 }
+
 function scrollR(father){
     $(`#${father}`)
     .children(".scroll-effect")
@@ -74,52 +112,3 @@ function scrollR(father){
     .children(".scroll-effect")
     .children(".product-cards-cotainer").scrollLeft() - 150);
 }
-
-//  function createProductOnly(Name, Price, Img, Class, Id) {
-//      // criar div por classe
-//      if(document.getElementById(Class) == null){
-//          $("main").append(`
-//          <div class="product-container-only" id="${Class}">
-//              <div class="product-text">
-//                  <a href="/section/${Class}"><h3>${Class}</h3></a>
-//              </div>
-//              <div class="scroll-effect">
-//                  <div class="product-cards-cotainer-only">
-//                  </div>
-//          </button>
-//              </div>
-//          </div>`
-//          );
-//      }
-//      addProductOnly(Name, Price, Img, Class, Id);
-//  }
-//  function addProduct(Name, Price, Img, Class, Id){
-//      $(`#${Class}`).children(".scroll-effect").children(".product-cards-cotainer")
-//      .append(`
-//          <a class="product-cards" href="/product/${Id}">
-//              <div class="img-container">
-//                  <img src="/${Img}" alt="">
-//              </div>
-//              <div class="product-price">
-//                  <p class="price">${Price}</p>
-//                  <p class="name">${Name}</p>
-//              </div>
-//              <button class="product-button">Adicionar</button>
-//          </a>
-//      `)
-//  }
-//  function addProductOnly(Name, Price, Img, Class, Id){
-//      $(`#${Class}`).children(".scroll-effect").children(".product-cards-cotainer-only")
-//      .append(`
-//          <a class="product-cards-only" href="/product/${Id}">
-//              <div class="img-container">
-//                  <img src="/${Img}" alt="">
-//              </div>
-//              <div class="product-price">
-//                  <p class="price">${Price}</p>
-//                  <p class="name">${Name}</p>
-//              </div>
-//              <button class="product-button">Adicionar</button>
-//          </a>
-//      `)
-//  }
